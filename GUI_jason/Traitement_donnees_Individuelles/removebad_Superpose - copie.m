9958df4db945b27d56f3f3b465e431171f35acaf
Jason Bouffard <jason.bouffard@mail.mcgill.ca>
2017-09-01 23:37:46 -0400

    copy

--- GUI_jason/Traitement_donnees_Individuelles/removebad_Superpose - copie.m
+++ GUI_jason/Traitement_donnees_Individuelles/removebad_Superpose - copie.m
@@ -1,42 +1,54 @@
 
-function data.Cycles_Table = removebad_Superpose( data, signal, cycles, type, varargin )
+function bad_cycles = removebad_Superpose( data, signal, cycles, type)
 
+% Lorsque cette fonction est utilisé dans un fichier de groupe, il y a
+% seulement un signal
 if strcmp(type, 'Group')
     data.Table1=data;
-    data.Cycle_Table(cycles,1)=1;
-    data.Cycle_Table(cycles,2)=duree(cycles);
     data.chan_name=signal;
+    
+    if exist('flagDuree','var')
+    data.Cycle_Table(cycles,1) = 1;
+    data.Cycle_Table(cycles,1)=dureecycles;
+    end
+    
 end
 
 numchan=length(signal);
 
-j=0;
+
 for isignal=1:numchan
-    j=j+1;
-    validnum(isignal)=find(strcmp(data.chan_name,signal{i})==1);
     
-    top(j)=max(data.(['Table' num2str(validnum(isignal))])(:,cycles));
-    bottom(j)=min(data.(['Table' num2str(validnum(isignal))])(:,cycles));
+    % Trouve le numéro de Table correspondant au signal d'intérêt
+    validnum(isignal)=find(strcmp(data.chan_name,signal{isignal})==1);
+    
+    %Déterminer la limite supérieure et inférieure du signal pour les axes
+    top(isignal)=max(data.(['Table' num2str(validnum(isignal))])(:,cycles));
+    bottom(isignal)=min(data.(['Table' num2str(validnum(isignal))])(:,cycles));
     
 end
 
-%Procedure Baseline
 figure(1)
 clf
-j=0;
-for isignal=1:numchan
+
+%% Initialise the graph
+for isignal=1:numchan % Plot each signal
+    
     subplot(numchan+1,1,isignal) %numchan2+1
     h(cycles,isignal)=plot(data.(['Table' num2str(validnum(isignal))])(:,cycles))
     
-    set(h(cycles,j),'color','b')
+    set(h(cycles,isignal),'color','b')
     
     hold on;
     
     a=axis;
-    axis([0 data.Cycle_Table(icycle,2)-data.Cycle_Table(i,1) bottom(j) top(j)])
+    axis([a(1) a(2) bottom(isignal) top(isignal)])
     
 end %For
 
+% Plot length of each trial if flag in varargin
+if exist('flagDuree', 'var')
+    
 for icycle=cycles
     
     subplot(numchan+1,1,numchan+1)
@@ -47,8 +59,27 @@ for icycle=cycles
     
 end
 
+end
+
+% Plot mean value of Table1 if flag in varargin
+if exist('flagMean', 'var')
+    
+for icycle=cycles
+    
+    subplot(numchan+1,1,numchan+1)
+    h(icycle,numchan+1)=plot(icycle,mean(data.Table1(:,icycle)));
+    hold on
+    
+    set(h(i,numchan+1),'color','b','marker','o');
+    
+end
+
+end
+
+
 xlabel('click in the white space when finished');
 
+%% clean data
 bad_cycles=[];
 count=0;
 over=0;
@@ -86,8 +117,5 @@ while not(over)
         over=1;
     end; %if
 end; %while
-
-data.Cycle_Table(:,3)=1;
-data.Cycle_Table(bad_cycles,3)=0;
 
 close(figure(1))
