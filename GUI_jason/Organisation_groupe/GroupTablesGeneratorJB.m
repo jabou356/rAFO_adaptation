83b996fd59b938ab9582d43cb71cd00f00fe95b8
Unknown <jason.bouffard@mail.mcgill.ca>
2018-03-19 23:54:04 -0400

    Adapt GroupDataGenerator for cells

--- GUI_jason/Organisation_groupe/GroupTablesGeneratorJB.m
+++ GUI_jason/Organisation_groupe/GroupTablesGeneratorJB.m
@@ -16,65 +16,55 @@ if Question==1
     load([pathname,filename])
     
     % Détermine le nombre de sujet déjà entré dans le fichier (Max n=30)
-    s=['tempN=find(isnan(GroupData.',cell2mat(Signal(1)),'(1,1,:))==1)'];eval(s);
-     clear s ;
-    N=tempN(1)-1;
+    N = size(GroupData.Signal{1},3);
+    %s=['tempN=find(isnan(GroupData.',cell2mat(Signal(1)),'(1,1,:))==1)'];eval(s);
+     %clear s ;
+    %N=tempN(1)-1;
   
     
 else
     
-    for i=1:length(Signal)
-    %Si vous n'avez pas de fichier de groupe, en crée un
-    tempSignal=cell2mat(Signal(i));
+%     for i=1:length(Signal)
+%     %Si vous n'avez pas de fichier de groupe, en crée un
+%     tempSignal=cell2mat(Signal(i));
     N=0;
-    s=['GroupData.',tempSignal,'(1:1500,1:1200,1:30)=nan'];eval(s);
-     clear s 
-    end
+%     s=['GroupData.',tempSignal,'(1:1500,1:1200,1:30)=nan'];eval(s);
+%      clear s 
+%     end
     
-    GroupData.Cycle_Table(1:5,1:1200,1:30)=nan;
+%     GroupData.Cycle_Table(1:5,1:1200,1:30)=nan;
    
 end
 
-Question=menu('Avez vous des fichiers à intégrer?','oui', 'non')
+Question=menu('Avez vous des fichiers à intégrer?','oui', 'non');
 
 while Question==1
     
-    clear Table* chan_name Cycle_Table ISRFLX_channel numchan num* s templenght onsetFF
+    clear Table chan_name Cycle_Table ISRFLX_channel numchan num* s templenght onsetFF
     N=N+1;
     
     % Va chercher le fichier de donnée du sujet N
     [filename,pathname]=uigetfile('*.mat','Sélectionnez votre fichier contenant votre table de données')
     load([pathname,filename])
       
-    for i=1:length(Signal)
+    for isignal=1:length(Signal)
         %Détermine le numéro de table du signal d'intérêt à partir du fichier
         %de calibration
-        tempSignal=cell2mat(Signal(i));
-        s=['numSignal=find(strcmp(chan_name(1,:),',char(39),tempSignal,char(39),')==1);'];eval(s);
-        clear s
+        numSignal=find(strcmp(chan_name(1,:),Signal{isignal}));
         
         %Va chercher les données dans la table du signal d'intérêt
-        s=['GroupData.',tempSignal,'(:,1:size(Table1,2),N)=Table',num2str(numSignal),'(1:1500,:);'];eval(s);
-        clear s
-        
-        if size(Table1,2)<1200
-            s=['GroupData.',tempSignal,'(:,size(Table1,2)+1:end,N)=nan'];eval(s);
-            clear s
-            
-        end
-        
+        GroupData.(Signal{isignal}){N} = cellfun(@(x)({x(:,numSignal)}),Table);
+         
+                
     end
     
-    GroupData.Cycle_Table(:,1:size(Table1,2),N)=Cycle_Table(:,:)';
-    GroupData.Cycle_Table(:,size(Table1,2)+1:end,N)=nan;
+    GroupData.Cycle_Table{N}=Cycle_Table(:,:)';
     
     
-    %Pad le reste du fichier avec des NAN pour garder une grandeur de table
-    %constante ( 1200 > que le nombre de cycles de marche)
+
     
     
-    
-    Question=menu('Avez vous des fichiers à intégrer?','oui', 'non')
+    Question = menu('Avez vous des fichiers à intégrer?','oui', 'non');
 end %while
 
 [filename,pathname]=uiputfile('*.mat');
