9fe55b8515a8d894c5d2e941842ad954c41bbc94
Unknown <jason.bouffard@mail.mcgill.ca>
2018-04-06 18:54:41 -0400

    Adapt RBI cells

--- GUI_jason/FctMatlab/Filter_RBI.m
+++ GUI_jason/FctMatlab/Filter_RBI.m
@@ -5,24 +5,36 @@ function fdata = FilterRBI(data,windowlength,type,dim)
 % window length: Length of the rbi window
 %type: 1=vector; 2= matrixè 3= 3D matrix
 %dim: dimension of the table corresponding to time
-
-dimension=size(data);
-data=abs(data);
-
-if type == 3
+if isnumeric(data)
+    dimension=size(data);
+    data=abs(data);
     
-fdata(1:dimension(1),1:dimension(2),1:dimension(3))=nan;
-
-    if dim == 1
-       
-        for itime=ceil(windowlength/2):dimension(1)-ceil(windowlength/2)
-            fdata(itime,:,:)=mean(data(itime-floor(windowlength/2):itime+floor(windowlength/2),:,:),1);
+    
+    
+    if type == 3
+        
+        fdata(1:dimension(1),1:dimension(2),1:dimension(3))=nan;
+        
+        if dim == 1
+            
+            for itime=ceil(windowlength/2):dimension(1)-ceil(windowlength/2)
+                
+                fdata(itime,:,:)=mean(data(itime-floor(windowlength/2):itime+floor(windowlength/2),:,:),1);
+                
+            end
+        end
+        
+    end
+elseif iscell(data)
+    for isubject = length(data):-1:1
+        for istride = length(data{isubject}):-1:1
+        for itime=ceil(windowlength/2):length(data{isubject}{istride})-ceil(windowlength/2)
+            
+            fdata{isubject}{istride}(itime,:)=mean(abs(data{isubject}{istride}(itime-floor(windowlength/2):itime+floor(windowlength/2))));
+        end
         end
     end
-    
-end
-
- 
+    end
 
 
 
