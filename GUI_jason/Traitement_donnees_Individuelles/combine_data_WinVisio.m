6547df6c09bf6461a2b6f38713e7dd3e78d0ba3d
Unknown <jason.bouffard@mail.mcgill.ca>
2018-04-06 21:46:50 -0400

    Clean and comment combine_data_WinVisio

--- GUI_jason/Traitement_donnees_Individuelles/combine_data_WinVisio.m
+++ GUI_jason/Traitement_donnees_Individuelles/combine_data_WinVisio.m
@@ -1,112 +1,71 @@
-%programme combine data
+function combine_data_WinVisio(config)
+%COMBINE_DATA_WINVISIO This function is used to combine multiple data files
+%extracted from Winvisio using Read_Winvisio_LB. Moreover, data are
+%filtered based on config files. 
+%   INPUT: Config file generated with XXX FUNCTION. Data files are selected
+%   in the function
+%   OUTPUT: None. Combined raw and filtered data are saved in
+%   combined_data.mat in the current directory.
 
-%amelioration de voir_reflex avec validation en continu et synchro un cycle
-%sur deux (18 juin 2012) Amélioration, enlève moyenne EMG JASON(19 novembre
-%2012)
-function combine_data_WinVisio
-clear all;
-
-verification=menu('Avez-vous un fichier de calibration et êtes-vous dans le bon dossier de données?','oui','non');
-
-if verification ==2
-    plante
-end
-
-
-    
-[fn,pn]=uigetfile('*.mat','select the calibration file')
-
-%% Load calibration
-load([pn,fn],'-mat');
-
-
-%% data transformation
-k=menu('data will now be prepared','OK');
-
+%% initiate loop variables 
 count=0; %number of files transformed
-choice=1;
-rawdata=[];
+choice=1; %menu('Process another data file?','yes','no');
+%% While user want to select other files
 while choice==1
     count=count+1;
+    
+    % Get the data file
     [fn,pn]=uigetfile('*.*','select a data file');
     load([pn,fn],'-mat');
     
-        rawdata=data([EMG_channels, Other_channels],:)';
-        data=rawdata;
-        data(1,1)=data(2,1);
-  
-    
-       
-    
-    %calibrate data
-    data=double(data);
-    rawdata=double(rawdata);
-    %+++++++++++ICI ON POURRAIT CALIBRER LES DONNEES
-    
-    numchan=size(data,2); %calcule le nombre de canaux
-    
+    %% Keep only selected channels, put EMG first
+        data=double(data([config.EMG_channels, config.Other_channels, config.Square_channels],2:end))'; %set to double, and ignore first data point (bad in Winvisio)
+        rawdata{count} = data;
+    %% Preprocess data
     %filter emg
-    h=waitbar(0.5,'Filtering EMG... please wait');
-    emgdata=data(:,1:size(EMG_channels,2)); %filtrer juste l'EMG
     
-    for i=1:size(EMG_channels,2)
-        data(:,i)=data(:,i)-mean(data(:,i),1);
-    end
+   % emgdata=data(:,1:size(EMG_channels,2)); %filtrer juste l'EMG
     
-    Fc=EMG_filter;
-    order=2;
+   % for iemg=1:size(EMG_channels,2)
+    %    data(:,iemg)=data(:,iemg)-mean(data(:,iemg),1);
+  %  end
+      
+    %% process EMG data
+    nemg = length(config.EMG_channels);
+    % rebase EMG
+    data(:,1:nemg) = data(:,1:nemg)- mean(data(:,1:nemg));
+    
+    % Filter EMG data
+    Fc=config.EMG_filter;
+    order=config.Order;
     [b,a] = butter(order,Fc/500);
-    fdata=filtfilt(b,a,emgdata);
+    fdata{count}=filtfilt(b,a,data(:,1:nemg));
     
-    fdata_display=fdata;
     %fdata=abs(fdata); %keep none rectified EMG as long as possible
-    close(h);
     
-    %filter other channels
-    h=waitbar(0.5,'Filtering other channels... please wait');
-    otherdata=data(:,size(EMG_channels,2)+1:size(EMG_channels,2)+size(Other_channels,2)); %filtrer juste les canaux non EMG
-    Fc=Other_filter;
-    order=2;
+    %% filter other channels
+    nothers = length(config.Other_channels);
+    
+    % Filter Other data
+    Fc=config.Other_filter;
+    order=config.Order;
     [b,a] = butter(order,Fc/500);
-    fdata2=filtfilt(b,a,otherdata);
-    close(h);
+    fdata{count}=[fdata{count},filtfilt(b,a,data(:,nemg+1:nemg+nothers))];
     
+    %% Do not filter square channels
+    nsquare = length(config.Square_channels);
+    fdata{count}=[fdata{count},data(:,nemg+nothers+1:nemg+nothers+nsquare)];
     
-    %combine all channels
-    fdata=[fdata,fdata2];
-    fdata_display=[fdata_display,fdata2];%so that we don<t see rectified EMG
-
-    
-    %save data temporarily
-    save(['temp_',num2str(count)],'fdata','rawdata');
-   fdata=[];
-    clear data;
-    rawdata=[];;
-    
-    %ask to continue
     choice=menu('Process another data file?','yes','no');
-end; %while
+end %while
 
-%% data combination
-choice=menu('Do you want to combine data files?','yes','no');
-if choice==1 %combine temp_n files
-    tempdata=[];
-    temprawdata=[];
-    for i=1:count %number of files to be combined
-        s=['load(''temp_',num2str(i),''');']; eval(s);
-                        
-        tempdata=[tempdata;fdata];
-        temprawdata=[temprawdata;rawdata];
-        
-    end;
+%% Concatenate all imported data files
+    fdata=cat(1,fdata{:});
+    rawdata=cat(1,rawdata{:});
     
-        fdata=tempdata;
-        rawdata=temprawdata;
-        clear tempdata;
-        save('combined_data','rawdata','fdata');
-else %no combination
-    load('temp_1'); %load temp file containing fdata
-    save('combined_data','rawsata','fdata'); % so from here loading combined data is sufficient
+%% Save concatenated filtered and raw data       
+    save('combined_data','rawdata','fdata');
+
 end
     
 
