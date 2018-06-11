c12c62e64d25c8258843ff105ecf609afbca49fe
Unknown <jason.bouffard@mail.mcgill.ca>
2018-06-11 18:42:32 -0400

    Proprio analysis continued

--- GUI_jason/Proprio/ProprioAnalysis.m
+++ GUI_jason/Proprio/ProprioAnalysis.m
@@ -13,15 +13,15 @@ chan_COUPLE=find(strcmp(config.chan_name,config.COUPLEName));
 chan_CONSF=find(strcmp(config.chan_name,config.ForcecommandName));
 chan_Response=find(strcmp(config.chan_name,config.ResponseName));
 
-% Find strides with an activated button 
-AnalProprio.response = cellfun(@(x)(min(x(:,chan_Response))<config.ResponseTh),Table);
+% Find strides with an activated button & consider a positive response if
+% the button was clicked during the current or the following stride & if the
+% stride is not valid  the button is not valid
+response = cellfun(@(x)(min(x(:,chan_Response))<config.ResponseTh),Table);
+AnalProprio.response = response(1:end-2)+response(2:end);
 AnalProprio.response(Cycle_Table(:,3)==0) = nan;
 
 
-%% Validate synchronisation
-% PUT REMOVE BAD SUPERPOSE HERE
-Cycle_Table(mauvaissync,3)=-1; %Add JB July13
-Cycle_Table(bad_cycles,3)=-1;
+SyncData = SyncPushoffProprio (Cycle_Table,data,condStrides, chan_ENCO);
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% end synchronisation on pushoff
@@ -59,212 +59,111 @@ for istride=1:length(condStrides{icond})
     
 end
 
+%% Validate synchronisation
+% PUT REMOVE BAD SUPERPOSE HERE
+Cycle_Table(mauvaissync,3)=-1; %Add JB July13
+Cycle_Table(bad_cycles,3)=-1;
+
 if icond == 1
 AnalProprio.baselineENCO=nanmean(AnalProprio.ENCO.(conditions{icond}),2);
 AnalProprio.baselineCOUPLE=nanmean(AnalProprio.COUPLE.(conditions{icond}),2);
 end
-end
-%% Rendu ICITTE 2018-05-29
 
-for i=1:length(STIM)
-    % ***********ATTENTION MODIFIE PAR LB POUR REMETTRE GONIO A ZERO AU
-    % maxCF
+% I keep baseline to have an idea of the specificity of FF induced
+% deviation
+for istride=1:length(condStrides{icond})
     
-    
-    %definit le max de deviation gonio apres le max de configne en force
-    
-    if Cycle_Table(STIM(i),3)==1
-        %plot results
-        %plot results
-        clf;
-        subplot(4,1,2);
-        plot(a,'r');
-        hold on;
+    % Find max kinematic deviation after FF onset
+    if Cycle_Table(condStrides{icond}(istride),3)==1
 
-        s=['a=Table',num2str(chan_CONSF),'(synctiming(STIM(i)):end,STIM(i));'];eval(s);
-        clf;
-        subplot(4,1,2);
-        plot(a,'r');
-        hold on;
+        %Find onset FF = 1st instance CONS_F cross detection threshold
+        AnalProprio.onsetFF.(conditions{icond})(istride) =find(abs(AnalProprio.CONS_F.(conditions{icond})(:,istride)) ...
+            >= config.RFLXdetect_level ,1,'first'); 
         
-        CONS_F(1:length(a),i)=a; %Add by JB July 8th 2015
-        [onsetCF(i), y]=find(abs(a)>=0.4,1,'first');%ATTENTION: plus le max, mais le seuil de debut du CF
-        themaxCF=find(abs(a)==max(abs(a)));
-        CF(i)=a(themaxCF(1));
+        % Find max FF and index
+        [AnalProprio.maxFF.(conditions{icond})(istride), AnalProprio.maxFFtiming.(conditions{icond})(istride)] = ...
+            max(abs(AnalProprio.CONS_F.(conditions{icond})(:,istride))); 
         
-        zz=axis;
-        plot([onsetCF(i) onsetCF(i)],[zz(3) zz(4)],'r:');
-        ylabel('Consigne');
+
+        % compute delta ENCO and deltaCOUPLE for each stride
+        AnalProprio.deltaENCO.(conditions{icond})(:,istride) = ...
+            AnalProprio.ENCO.(conditions{icond})(:,istride) - AnalProprio.baselineENCO;
         
-        tempref_ENCO=ref_ENCO-ref_ENCO(onsetCF(i));
+        AnalProprio.deltaCOUPLE.(conditions{icond})(:,istride) = ...
+            AnalProprio.COUPLE.(conditions{icond})(:,istride) - AnalProprio.baselineCOUPLE;
         
+        % compute corrected deltaENCO and deltaCOUPLE: bring deltaENCO and deltaCOUPLE to zero at onsetFF
+        AnalProprio.deltaENCOcorr.(conditions{icond})(:,istride) = ...
+            AnalProprio.deltaENCO.(conditions{icond})(:,istride) - ...
+            AnalProprio.deltaENCO.(conditions{icond})(AnalProprio.onsetFF.(conditions{icond})(istride),istride);
         
+        AnalProprio.deltaCOUPLEcorr.(conditions{icond})(:,istride) = ...
+            AnalProprio.deltaCOUPLE.(conditions{icond})(:,istride) - ...
+            AnalProprio.deltaCOUPLE.(conditions{icond})(AnalProprio.onsetFF.(conditions{icond})(istride),istride);
         
-        tempSTIM(1:750)=nan;
-        s=['tempSTIM=Table',num2str(chan_ENCO),'(synctiming(STIM(i)):end,STIM(i));'];eval(s);
-        tempSTIM2=tempSTIM-tempSTIM(onsetCF(i));
-        
-        subplot(4,1,1);
-        hold on;
-        plot(tempref_ENCO);
-       %plot(ref_ENCO);
-        hold on;
-        plot(tempSTIM2,'r')
-       %plot(tempSTIM,'r')
-        
-        if length(tempSTIM)<length(ref_ENCO)
-            delta1=tempSTIM-ref_ENCO(1:length(tempSTIM));
-            delta2=tempSTIM2-tempref_ENCO(1:length(tempSTIM));
-        else
-            delta1=tempSTIM(1:length(ref_ENCO))-ref_ENCO;
-            delta2=tempSTIM2(1:length(ref_ENCO))-tempref_ENCO;
-        end
-        
-        plot(delta2,'g');
-        ylabel('Gonio');
-        
-        deltaENCO(1:length(delta1),i)=delta1; 
-        deltaENCOcorrected(1:length(delta2),i)=delta2; 
-                                      
-        %retour en arriere et trouve le max de deviation APRES le max de
-        %consigne en force
-        
-        %ICI!!!!!********
-         subplot(4,1,1);
-         
-        x=find(isnan(delta1)==0);
         
         if Direction==1
-            themax1(i)=find(delta1(onsetCF(i)+1:x(end))==min(delta1(onsetCF(i)+1:x(end))),1,'first');
-            themax2(i)=find(delta2(onsetCF(i)+1:x(end))==min(delta2(onsetCF(i)+1:x(end))),1,'first');%themax=find((delta(themaxCF+1:end))==max(abs(delta(themaxCF+1:end)))); chang? par JB remove abs
-        elseif Direction==2
-            themax1(i)=find(delta1(onsetCF(i)+1:x(end))==max(delta1(onsetCF(i)+1:x(end))),1,'first');
-            themax2(i)=find(delta2(onsetCF(i)+1:x(end))==max(delta2(onsetCF(i)+1:x(end))),1,'first');
-        end
-        
-        themax2(i)=themax2(i)+onsetCF(i);
-        themax1(i)=themax1(i)+onsetCF(i);
-        
-        zz=axis;
-        plot([themax2(i) themax2(i)],[zz(3) zz(4)],'r:');
-        
-        erreur_max1(i)=delta1(themax1(i));%Erreur maximal sur rawdata
-        erreur_max2(i)=delta2(themax2(i));%Erreur maximal dont le offset lors du onsetCF a été retiré
-        xlabel(erreur_max2(i));
-        
-        subplot(4,1,3);
-        s=['a=Table',num2str(chan_COUPLE),'(synctiming(STIM(i)):end,STIM(i));'];eval(s);
-        
-        ref_COUPLE=ref_COUPLE-ref_COUPLE(onsetCF(i)); %%%%%%2017 ATTENTION: corrigé pour vertical displacement de couple!!!!!
-        a=a-a(onsetCF(i)); %%%%%%2017 ATTENTION: corrigé pour vertical displacement de couple!!!!!
-        
-        plot(ref_COUPLE);
-        hold on;
-        
-        plot(a,'r');
-                    
-        
-        if length(a)<length(ref_COUPLE)
-            deltaF=a-ref_COUPLE(1:length(a));
-        else
-            deltaF=a(1:length(ref_COUPLE))-ref_COUPLE;   
-        end
-        
-        plot(deltaF,'g');
-        ylabel('Force');
-        
-        deltaCOUPLE(1:length(deltaF),i)=deltaF; %%%%%%2017 ATTENTION: MAINTENANT CA DONNE corrigé pour vertical displacement de couple!!!!!
-
-        
-        if Direction==1
-            maxdeltaF(i)=min(deltaF(onsetCF(i)+1:x(end)));
-            maxF(i)=min(a(onsetCF(i)+1:x(end)));
-            
+            % If FF is toward plantarflexion, find peakDeltaENCO (MIN) and its
+            % index
+            [AnalProprio.peakDeltaENCO.(conditions{icond})(istride), temptiming] = ...
+               min(AnalProprio.deltaENCO.(conditions{icond})(AnalProprio.onsetFF.(conditions{icond})(istride):end,istride));
+           
+           AnalProprio.peakDeltaENCOtiming.(conditions{icond})(istride) = temptiming + AnalProprio.onsetFF.(conditions{icond})(istride) -1;
+           
+           % If FF is toward plantarflexion, find peakDeltaENCO (MIN) corrected for delta ENCO at onsetFF and its
+            % index
+           [AnalProprio.peakDeltaENCOcorr.(conditions{icond})(istride), temptiming] = ...
+               min(AnalProprio.deltaENCOcorr.(conditions{icond})(AnalProprio.onsetFF.(conditions{icond})(istride):end,istride));
+           
+           AnalProprio.peakDeltaENCOcorrtiming.(conditions{icond})(istride) = temptiming + AnalProprio.onsetFF.(conditions{icond})(istride) -1;
+           
+           % If FF is toward plantarflexion, find peakDeltaCOUPLE (MIN) and its
+            % index
+            [AnalProprio.peakDeltaCOUPLE.(conditions{icond})(istride), temptiming] = ...
+               min(AnalProprio.deltaCOUPLE.(conditions{icond})(AnalProprio.onsetFF.(conditions{icond})(istride):end,istride));
+           
+           AnalProprio.peakDeltaCOUPLEtiming.(conditions{icond})(istride) = temptiming + AnalProprio.onsetFF.(conditions{icond})(istride) -1;
+           
+           % If FF is toward plantarflexion, find peakDeltaCOUPLE (MIN) corrected for delta ENCO at onsetFF and its
+            % index
+           [AnalProprio.peakDeltaCOUPLEcorr.(conditions{icond})(istride), temptiming] = ...
+               min(AnalProprio.deltaCOUPLEcorr.(conditions{icond})(AnalProprio.onsetFF.(conditions{icond})(istride):end,istride));
+           
+           AnalProprio.peakDeltaCOUPLEcorrtiming.(conditions{icond})(istride) = temptiming + AnalProprio.onsetFF.(conditions{icond})(istride) -1;
+       
         elseif Direction==2
             
-            maxdeltaF(i)=max(deltaF(onsetCF(i)+1:x(end)));
-            maxF(i)=max(a(onsetCF(i)+1:x(end)));
+            % If FF is toward dorsiflexion, find peakDeltaENCO (MAX) and its
+            % index
+            [AnalProprio.peakDeltaENCO.(conditions{icond})(istride), temptiming] = ...
+               max(AnalProprio.deltaENCO.(conditions{icond})(AnalProprio.onsetFF.(conditions{icond})(istride):end,istride));
+           
+           AnalProprio.peakDeltaENCOtiming.(conditions{icond})(istride) = temptiming + AnalProprio.onsetFF.(conditions{icond})(istride) -1;
+           
+           % If FF is toward dorsiflexion, find peakDeltaENCO (MAX) corrected for delta ENCO at onsetFF and its
+            % index
+           [AnalProprio.peakDeltaENCOcorr.(conditions{icond})(istride), temptiming] = ...
+               max(AnalProprio.deltaENCOcorr.(conditions{icond})(AnalProprio.onsetFF.(conditions{icond})(istride):end,istride));
+           
+           AnalProprio.peakDeltaENCOcorrtiming.(conditions{icond})(istride) = temptiming + AnalProprio.onsetFF.(conditions{icond})(istride) -1;
+           
+           % If FF is toward plantarflexion, find peakDeltaCOUPLE (max) and its
+            % index
+            [AnalProprio.peakDeltaCOUPLE.(conditions{icond})(istride), temptiming] = ...
+               max(AnalProprio.deltaCOUPLE.(conditions{icond})(AnalProprio.onsetFF.(conditions{icond})(istride):end,istride));
+           
+           AnalProprio.peakDeltaCOUPLEtiming.(conditions{icond})(istride) = temptiming + AnalProprio.onsetFF.(conditions{icond})(istride) -1;
+           
+           % If FF is toward plantarflexion, find peakDeltaCOUPLE (max) corrected for delta ENCO at onsetFF and its
+            % index
+           [AnalProprio.peakDeltaCOUPLEcorr.(conditions{icond})(istride), temptiming] = ...
+               max(AnalProprio.deltaCOUPLEcorr.(conditions{icond})(AnalProprio.onsetFF.(conditions{icond})(istride):end,istride));
+           
+           AnalProprio.peakDeltaCOUPLEcorrtiming.(conditions{icond})(istride) = temptiming + AnalProprio.onsetFF.(conditions{icond})(istride) -1;
         end
         
-         
-        
-        peakFtiming(i)=find(deltaF==maxdeltaF(i),1,'first');
-        
-        
-
-        
-         s=['a=Table',num2str(chan_Response),'(synctiming(STIM(i)):end,STIM(i));'];eval(s);
-         bouton(1:length(a),i)=a;
-         s=['a=Table',num2str(chan_Response),'(1:end,STIM(i)+1);'];eval(s);
-         bouton(751:750+length(a),i)=a;
-         
-         subplot(4,1,4);
-         title(num2str(reponse(i)))
-         hold on
-        
-        plot(bouton(:,i));
-        ylabel('reponse');
-        
-        waitforbuttonpress
-
         
     end%if;
 end %for
-resultat(:,1)=CF';
-resultat(:,3)=maxdeltaF';
-resultat(:,2)=maxF';
-resultat(:,4)=erreur_max1';
-resultat(:,5)=erreur_max2';
-resultat(:,6)=reponse';
+end
 
-variable.erreurmax1timing=themax1;
-variable.onsetCF=onsetCF;
-variable.erreurmax2timing=themax2;
-variable.peakFtiming=peakFtiming;
-variable.deltaCOUPLE=deltaCOUPLE; %LB 2017 variable de sortie est maintenant couple corrigé
-variable.deltaENCO=deltaENCO;
-variable.deltaENCOcorrected=deltaENCOcorrected;
-variable.CONS_F=CONS_F;
-variable.bouton=bouton;
-
-%fn=strrep(fn,'.mat','.xls');
-xlswrite('AnalProprio',resultat,'A2');
-% xlswrite('AnalProprio',{'CONS_F'},'A1');
-% xlswrite('AnalProprio',{'COUPLE'},'B1');
-% xlswrite('AnalProprio',{'deltaCOUPLE'},'C1');
-% xlswrite('AnalProprio',{'deltaENCO'},'D1');
-% xlswrite('AnalProprio',{'deltaENCOcorrected'},'E1');
-% xlswrite('AnalProprio',{'réponse'},'F1');
-
-
-
-%% analyse
-
-
-% figure(3);
-% clf;
-% plot(resultats(1,:),resultats(2,:),'o');
-% ylabel('dev cheville');
-% xlabel('consigne en force');
-%
-% figure(4);
-% clf;
-% plot(resultats(3,:),resultats(2,:),'o');
-% ylabel('dev cheville');
-% xlabel('force appliquee');
-%
-% figure(5);
-% clf;
-% plot(resultats(1,:),resultats(3,:),'o');
-% ylabel('force appliquee');
-% xlabel('consigne en force');
-%
-% figure(6);
-% clf;
-% plot(resultats(2,:),resultats(5,:),'o');
-% ylabel('EMG change');
-% xlabel('dev cheville');
-%
-%
-% resultats=resultats';
-% xlswrite(fn,resultats);
